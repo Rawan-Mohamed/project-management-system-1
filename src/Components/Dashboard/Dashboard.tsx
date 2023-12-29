3d87ee74475e77367ea4ad46b5c7e615f87bcdc1
samiya <samii-1989@live.com>
2023-12-29 14:38:45 +0100

    "dark mode for charts"

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -3,12 +3,14 @@ import { useContext, useEffect, useState } from "react";
 import { ToastContext } from "../../Context/ToastContext";
 import { AuthContext } from "../../Context/AuthContext";
 import DonutChart from "react-donut-chart";
+import { ThemeContext } from '../../Context/ThemeContext';
 
 const Dashboard = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { userData, baseUrl, requestHeaders, userRole }: any =
     useContext(AuthContext);
   const { getToastValue }: any = useContext(ToastContext);
+  const {isDarkMode} = useContext(ThemeContext);
   const [todoCount, setTodoCount] = useState(0);
   const [progressCount, setProgressCount] = useState(0);
   const [doneCount, setDoneCount] = useState(0);
@@ -46,6 +48,7 @@ const Dashboard = () => {
         // console.log(error);
       });
   };
+  
 
   useEffect(() => {
     getTasksCount();
@@ -73,26 +76,26 @@ const Dashboard = () => {
               <h3  className="">Tasks</h3>
               <p>List of all tasks</p>
             </div>
-            <div className="row tasks-count mt-5">
+            <div className="row tasks-count mt-5 gy-3">
               <div className="col-md-4 ">
                 <div className="tasks-status d-flex flex-column text-center p-3  rounded-4">
                   <i className="fa fa-tasks my-3"> </i>
-                  <span className="py-3 text-dark">To do</span>
-                  <h3 className="text-dark">{todoCount}</h3>
+                  <span className="py-3 text-dark-light">To do</span>
+                  <h3 className="text-dark-light">{todoCount}</h3>
                 </div>
               </div>
               <div className="col-md-4 ">
                 <div className="tasks-status d-flex flex-column text-center p-3 rounded-4">
                   <i className="fa-solid fa-hourglass-end my-3"></i>
-                  <span className="py-3 text-dark">In-progress</span>
-                  <h3 className="text-dark">{progressCount}</h3>
+                  <span className="py-3 text-dark-light">In-progress</span>
+                  <h3 className="text-dark-light">{progressCount}</h3>
                 </div>
               </div>
               <div className="col-md-4">
                 <div className="tasks-status d-flex flex-column text-center  p-3  rounded-4">
                   <i className="fa-solid fa-clipboard-check my-3"></i>
-                  <span className="py-3 text-dark">Done</span>
-                  <h3 className="text-dark">{doneCount}</h3>
+                  <span className="py-3 text-dark-light">Done</span>
+                  <h3 className="text-dark-light">{doneCount}</h3>
                 </div>
               </div>
             </div>
@@ -103,19 +106,19 @@ const Dashboard = () => {
                 <h3>Users</h3>
                 <p>List of all users</p>
               </div>
-              <div className="row users-count mt-5">
+              <div className="row users-count mt-5 gy-3">
                 <div className="col-md-6">
                   <div className="  status d-flex flex-column text-center rounded-4 p-3">
                     <i className="fa-brands fa-creative-commons-by my-3"></i>
-                    <span className="py-3 text-dark">Active users</span>
-                    <h3 className="text-dark">{activeUserCount}</h3>
+                    <span className="py-3 text-dark-light">Active users</span>
+                    <h3 className="text-dark-light">{activeUserCount}</h3>
                   </div>
                 </div>
-                <div className="col-md-6">
+                <div className="col-md-6  ">
                   <div className=" status d-flex flex-column text-center rounded-4 p-3">
                     <i className="fa-brands fa-creative-commons-pd my-3"></i>
-                    <span className="py-3 text-dark">De-active users</span>
-                    <h3 className="text-dark">{inActiveUserCount}</h3>
+                    <span className="py-3 text-dark-light">De-active users</span>
+                    <h3 className="text-dark-light">{inActiveUserCount}</h3>
                   </div>
                 </div>
               </div>
@@ -124,8 +127,8 @@ const Dashboard = () => {
             ""
           )}
         </div>
-        <div className="row chart my-4 ">
-          <div className="col-md-6  px-5">
+        <div className="row chart my-4 gy-3">
+          <div className="col-md-6  px-5  ">
             <div className="tasks-chart rounded-5 d-flex align-items-center justify-content-center pt-5">
               <DonutChart
                 data={[
@@ -138,7 +141,10 @@ const Dashboard = () => {
                 clickToggle={true}
                 interactive={true}
                 innerRadius= {0.6}
-                colors={["#CFD1EC", "#E4E4BC", "#E7C3D7"]}
+                colors = {isDarkMode ? ["#7187FF", "#9D9D9D", "#CFD1EC"]
+                : ["#CFD1EC", "#E4E4BC", "#E7C3D7"]}
+                className={isDarkMode && "recharts-label" }
+                // colors={["#CFD1EC", "#E4E4BC", "#E7C3D7"]}
               />
             </div>
           </div>
@@ -156,8 +162,10 @@ const Dashboard = () => {
                 clickToggle={true}
                 interactive={true}
                 innerRadius= {0.6}
-                colors={["#55ce63", "#EA5446"]}
-              />{" "}
+                colors = {isDarkMode ? ["#4EDB79", "#FF6961"] : ["#55ce63", "#EA5446"]}
+                // colors={["#55ce63", "#EA5446"]}
+                className={isDarkMode && "recharts-label" }
+              />
             </div>
           </div>
           ):''}
